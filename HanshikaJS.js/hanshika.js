a50e0cae257fc5db5d7d5d0daef7c3f9cca40b75
Hanshika Jain <hanshikapatni@gmail.com>
2026-01-28 21:56:17 +0530

    Initial commit with Unit 1–5 practicals and portfolio

---
+++ HanshikaJS.js/hanshika.js
@@ -0,0 +1,7 @@
+let arr=[2,3,4,5,6,];
+let newArr= arr.map(
+    (val)=>{
+        return val*2;
+    }
+);
+console.log(newArr);
---
+++ nodescript.js
@@ -0,0 +1,5 @@
+let marks = [100, 90, 97, 98, 94, 95];
+let sum = 0;
+for (let val of marks) {
+    console.log(val);
+}
